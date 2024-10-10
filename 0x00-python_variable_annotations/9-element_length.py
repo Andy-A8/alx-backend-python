37aff02885d502cf895ea792b502ff1dd6f237b9
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-10-10 14:28:01 -0700

    Annotate function to return appropriate types

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Annotate the parameters of the function and
+    return values with the appropriate types
+
+def element_length(lst):
+    return [(i, len(i)) for i in lst]
+"""
+
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples"""
+    return [(i, len(i)) for i in lst]
